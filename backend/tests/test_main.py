826b49c81723b89bdc6fa0494a445c99ce5a474c
Mateusz Karoń <mateuszkaron96@gmail.com>
2025-12-08 22:17:30 +0100

    Poprawka tekstu w tescie health check

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Witaj w API Wypożyczalni Video"}
+    assert response.json() == {"message": "API Wypożyczalni działa poprawnie"}
